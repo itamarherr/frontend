db6bbd5a7b947764715c219ab8b6c2a8a1179ff1
Itamar Herr <itamarherr@gmail.com>
2025-01-24 22:40:07 +0200

    Footer modifications

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 
 const Footer = () => {
-  return <div
-className="p-6 flex gap-3 bg-fuchsia-50 text-fuchsia-900 dark:text-fuchsia-50 shadow-md"
-style={{
-    boxShadow: "0px -20px 20px rgba(0, 0, 0, 0.25)",
-  }}
- >
-    <img
+  return (
+    <div
+      className="p-8 flex flex-col md:flex-row items-center justify-between gap-6 bg-fuchsia-50 text-fuchsia-900 dark:text-fuchsia-50"
+      style={{
+        boxShadow: "0px -20px 20px rgba(0, 0, 0, 0.25)",
+      }}
+    >
+      <div className="text-center md:text-left flex flex-col md:flex-row md:items-center md:gap-6">
+      <img
         src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_960_720.png"
         alt="Eco Logo"
-        style={{ height: "80px", width: "auto" }}
+        className="h-20 w-auto"
       />
-      <h1 style={{ fontSize: "40px" }}>Eco Services</h1>
-    </div>;
+      <h1 className="text-4xl ">Eco Services</h1>
+      <p className="text-sm text-gray-500">@ 2025 All righta reserved</p>
+      </div>
+      <div className="text-center md:text-left flex flex-col md:flex-row md:items-center md:gap-6">
+      
+      <p className="text-lg mt-2">Phone: <a href="tel:+972 523278700" className="text-blue-500 hover:underline">+972 523278700</a></p>
+      <p className="text-lg mt-2">Email: <a href="maito:itamarherr@gmail.com" className="text-blue-500 hover:underline">itamarherr@gmail.com</a></p>
+      </div>
+      
+      <button
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        className="mt-4 md:mt-0 p-3 bg-blue-500 text-white text-lg rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300"
+      >
+        Back to Top
+      </button>
+   </div>
+  );
 };
 
 export default Footer;
